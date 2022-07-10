e0d383d502c97f95bfe40a1411b9048d250365cd
Misha228550 <108177481+Misha228550@users.noreply.github.com>
2022-07-10 13:40:06 +0300

    Update shuter.py
    Карказ игры, класс Soldier

--- shuter.py
+++ shuter.py
@@ -1,1 +1,34 @@
 import pygame
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = (SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("Война в Сталинграде")
+
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self,x,y,scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load("img/player/Idle/0.png")
+        self.image = pygame.transform.scale(img,(img.get_width() * scale, img.get_height() * scale))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+
+    def draw(self):
+        screen.blit(self.image,self.rect)
+
+
+player = Soldier(200,300,2)
+player2 = Soldier(300,300,2)
+
+
+run = True
+while run:
+    player.draw()
+    player2.draw()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+    pygame.display.update()
