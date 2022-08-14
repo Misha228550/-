6f376a65630c58b1d8735068b6f29c71783b8b45
Misha228550 <mixailalexegorov@mail.ru>
2022-08-14 12:55:05 +0300

    Добавление прыжка, стен

---
+++ Names.py
@@ -0,0 +1,14 @@
+# ИМЯ
+def Name():
+    Names = []
+    i = "Василий Иванович Пётрушка"
+    for r in range(5):
+        Names.append(r)
+    print(Names)
+    for h in range(5):
+        ROP =Names.pop()
+        print(ROP)
+    print(Names)
+Name()
+
+
--- shuter.py
+++ shuter.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -9,21 +10,32 @@ pygame.display.set_caption("Война в Сталинграде")
 
 #Частота кадров
 clock = pygame.time.Clock()
-FPS = 120
+FPS = 60
+
+Gravitazya = 0.75 #Гравитация
 
 #Действия игрока
 moving_left = False
 moving_right = False
 
-BackRound = (32,74,209)
+BackRound = (32,74,209) #Заливка фона
+RET = (255,0,0) #Цвет красный
 
 def draw_backroundt():
     screen.fill(BackRound)
+    pygame.draw.line(screen,RET,(0,400),(SCREEN_WIDTH,400),5)
+    pygame.draw.line(screen, RET, (0, 0), (SCREEN_WIDTH, 1000000), 5)
+    pygame.draw.line(screen, RET, (0, 1000000), (SCREEN_WIDTH, 0), 5)
 
+#Класс игрока
 class Soldier(pygame.sprite.Sprite):
     def __init__(self,carector_tipe,x,y,scale,speed):
         # Загрузка изображения персонажа
         pygame.sprite.Sprite.__init__(self)
+        self.jump = False
+        self.jump_in_air = True
+        self.alive = True
+        self.vel_y = 0
         self.carector_tipe = carector_tipe
         self.speed=speed
         self.direction = 1
@@ -32,18 +44,15 @@ class Soldier(pygame.sprite.Sprite):
         self.frame_index=0
         self.action = 0
         self.updata_time = pygame.time.get_ticks()
-        tenp_list = []
-        for i in range(5):
-            img = pygame.image.load(f"img/{self.carector_tipe}/Idle/{i}.png").convert_alpha()
-            img = pygame.transform.scale(img,(img.get_width() * scale, img.get_height() * scale))
-            tenp_list.append(img)
-        self.animation_list.append(tenp_list)
-        tenp_list = []
-        for i in range(6):
-            img = pygame.image.load(f"img/{self.carector_tipe}/Run/{i}.png").convert_alpha()
-            img = pygame.transform.scale(img,(img.get_width() * scale, img.get_height() * scale))
-            tenp_list.append(img)
-        self.animation_list.append(tenp_list)
+        Animetion_Types = ["Idle","Run","Jump"]
+        for animation in Animetion_Types:
+            tenp_list = []
+            num_off_frames = len(os.listdir(f"img/{self.carector_tipe}/{animation}"))
+            for i in range(num_off_frames):
+                img = pygame.image.load(f"img/{self.carector_tipe}/{animation}/{i}.png").convert_alpha()
+                img = pygame.transform.scale(img,(img.get_width() * scale, img.get_height() * scale))
+                tenp_list.append(img)
+            self.animation_list.append(tenp_list)
         self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
@@ -72,10 +81,11 @@ class Soldier(pygame.sprite.Sprite):
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image,self.flip,False),self.rect)
+        #pygame.draw.rect(screen,RET,(self.rect.x,self.rect.y, self.rect.width,self.rect.height),3)
 
-
+    #Движение
     def move(self,moving_left,moving_right):
-        #Обнулить переменные перемещения
+        #Переменные перемещения
         dx=0
         dy=0
 
@@ -83,10 +93,31 @@ class Soldier(pygame.sprite.Sprite):
             dx = - self.speed
             self.direction = -1
             self.flip = True
+            if player.rect.left <= 0:
+                dx = 0
         if moving_right:
             dx = self.speed
             self.direction = 1
             self.flip = False
+            if player.rect.right >= 800:
+                dx = 0
+        if self.jump == True and self.jump_in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.jump_in_air = True
+
+        self.vel_y += Gravitazya
+        dy += self.vel_y
+
+        if self.rect.bottom + dy > 400:
+            dy = 400 - self.rect.bottom
+            self.jump_in_air = False
+
+
+
+
+
+
 
         #Обновление позиции игрока
         self.rect.x += dx
@@ -98,18 +129,24 @@ Zlodey = Soldier("enemy",300,300,2,3)
 
 
 run = True
+#Функции при запуске игры
 while run:
     draw_backroundt()
     player.draw()
     Zlodey.draw()
 
-    if moving_left or moving_right:
-        player.updata_actiones(1)
-    else:
-        player.updata_actiones(0)
+    #Обновление действий игрока
+    if player.alive: #Если игрок жив
+        if player.jump_in_air:
+            player.updata_actiones(2)
+        elif moving_left or moving_right:
+            player.updata_actiones(1)
+        else:
+            player.updata_actiones(0)
 
     player.move(moving_left, moving_right)
     player.updata_animations()
+
 
 
     for event in pygame.event.get():
@@ -122,6 +159,8 @@ while run:
                 moving_left = True
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_w and player.alive:
+                player.jump = True
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 quit()
@@ -130,11 +169,13 @@ while run:
                 moving_left = False
             if event.key == pygame.K_d:
                 moving_right = False
+            #if event.key == pygame.K_w and player.alive:
+                #player.jump = False
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 quit()
-        # Проверка нажатия клавиш
         # KEYUP - отжатие
         # KEYDOWN - нажатие
+    #Обновление экрана
     pygame.display.update()
     clock.tick(FPS)
